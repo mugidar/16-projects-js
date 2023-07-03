5d2984804227281d5ddef814132538944b9d363d
mugidar <mugidar@gmail.com>
2023-07-03 19:06:21 +0300

    chars counter

--- 4/main.js
+++ 4/main.js
@@ -6,10 +6,9 @@ const limit = 3
 leftEl.textContent = limit
 textareaEl.addEventListener("keyup", (e) => {
     console.log(e.target.value)
-    const leftChars = limit-e.target.value.length
     const count = e.target.value.length
     countEl.textContent = count
     leftEl.textContent =Number( textareaEl.getAttribute("maxLength")) - e.target.value.length
 
-    console.log()
+ 
 })
